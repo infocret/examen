519dfa10392ea905ca06e20704e072711a30b1ac
Infocret <julio.buendia@infocret.com>
2022-03-25 03:34:46 -0600

    SE agrego info

--- resources/views/mensajes/info_fields.blade.php
+++ resources/views/mensajes/info_fields.blade.php
@@ -1,26 +1,39 @@
 
 <div class="form-group">
     {!! Form::label('dowcod', 'Descargar codigo:') !!}
-    
+    <a href="https://drive.google.com/file/d/1zVOAipI-Z5q13XX4H89Q4XLYUUgS-Rgw/view?usp=sharing">
+          <span>Paquete de codigo de proyecto</span>
+	</a>
 </div>
 
 <div class="form-group">
     {!! Form::label('dowscript', 'Descargar script:') !!}
-    
+    <a href="https://drive.google.com/file/d/1fBAk4sRmZHBASReWO614rUJtp86nkI9_/view?usp=sharing">
+          <span>Script de Base de Datos</span>
+	</a>    
 </div>
 
 <div class="form-group">
-    {!! Form::label('dbrem', 'Acceso remoto a DB:') !!}
-    
+	{!! Form::label('dbrem', 'Acceso remoto a DB:') !!}
 </div>
+
+<div class="form-group">
+    {!! Form::label('dbrem', 
+    'hostname: 185.28.21.52, DB: u776939812_exam, user: u776939812_julio, pass: C&Aexam2022') !!}
+</div>
+
 
 <div class="form-group">
     {!! Form::label('gith', 'Repositorio Git:') !!}
-    
+    <a href="https://github.com/infocret/examen">
+          <span>Repositorio Git</span>
+	</a>     
 </div>
 
-<div class="form-group">
-    {!! Form::label('ytube', 'Video youtube:') !!}
-    
+<div class="form-group">       
+    {!! Form::label('ytube', 'Video YouTube:') !!}
 </div>
 
+<div class="form-group">       
+ <iframe width="560" height="315" src="https://www.youtube.com/embed/01J9FV_pchY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
